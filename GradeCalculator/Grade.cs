12f9863bdfb2dcf02f1599f995b5557deb48d73c
oikz <chapmanjgm@gmail.com>
2021-10-08 16:57:42 +1300

    Basic Display of grades for a course - Using a DataGrid to display and enable editing of cells

--- GradeCalculator/Grade.cs
+++ GradeCalculator/Grade.cs
@@ -2,10 +2,12 @@
     public class Grade {
         public string Name { get; set; }
         public double Result { get; set; }
+        public double Weight { get; set; }
 
-        public Grade(string name, double result) {
+        public Grade(string name, double result, double weight) {
             Name = name;
             Result = result;
+            Weight = weight;
         }
     }
 }
--- GradeCalculator/MainWindow.xaml.cs
+++ GradeCalculator/MainWindow.xaml.cs
@@ -19,6 +19,7 @@ namespace GradeCalculator {
     /// </summary>
     public partial class MainWindow : Window {
         private List<Course> courses;
+
         public MainWindow() {
             InitializeComponent();
 
@@ -27,31 +28,57 @@ namespace GradeCalculator {
 
             //Placeholder
             var course1 = new Course("Software Design", "SWEN225");
-            course1.Grades.Add(new Grade("Assignment 1", 100));
+            course1.Grades.Add(new Grade("Assignment 1", 100, 15));
+            course1.Grades.Add(new Grade("Assignment 2", 100, 15));
+            course1.Grades.Add(new Grade("Assignment 3", 100, 25));
             var course2 = new Course("Clouds and Networking", "NWEN243");
             courses = new List<Course> {course1, course2};
 
             Courses.ItemsSource = courses;
             Courses.SelectionChanged += ViewCourse;
         }
+
         private void ViewCourse(object sender, SelectionChangedEventArgs args) {
             //Show the course information
             MenuTitle.Visibility = Visibility.Hidden;
-            var items = new ListBox {
-                ItemsSource = courses[0].Grades, 
-                DisplayMemberPath = "Name",
+
+            
+            var grid = new DataGrid {
+                ItemsSource = courses[0].Grades,
+                AutoGenerateColumns = false,
+                HeadersVisibility = DataGridHeadersVisibility.Column,
+                BorderBrush = null,
+                FontSize = 20,
                 Background = Brushes.Black,
                 Foreground = Brushes.White,
-                BorderBrush = null,
-                FontSize = 25,
-                Width = 450,
-                HorizontalAlignment = HorizontalAlignment.Left
+                HorizontalGridLinesBrush = Brushes.White,
+                VerticalGridLinesBrush = Brushes.White,
+                CanUserResizeColumns = false,
+                CanUserResizeRows = false,
+                CanUserAddRows = false,
+                CanUserDeleteRows = false
             };
-            
-            
-            Grid.Children.Add(items);
-            Grid.SetColumn(items, 1);
 
+
+            grid.Columns.Add(new DataGridTextColumn {
+                Header = "Name", Binding = new Binding("Name"), Width = 275
+            });
+            grid.Columns.Add(new DataGridTextColumn {
+                Header = "Result", Binding = new Binding("Result"), Width = 75
+            });
+            grid.Columns.Add(new DataGridTextColumn {
+                Header = "Weight", Binding = new Binding("Weight"), Width = 75
+            });
+
+
+            grid.Columns[0].CellStyle = Resources["BlackCell"] as Style;
+            grid.Columns[0].HeaderStyle = Resources["BlackHeader"] as Style;
+            grid.Columns[1].CellStyle = Resources["BlackCell"] as Style;
+            grid.Columns[1].HeaderStyle = Resources["BlackHeader"] as Style;
+            grid.Columns[2].CellStyle = Resources["BlackCell"] as Style;
+            grid.Columns[2].HeaderStyle = Resources["BlackHeader"] as Style;
+            Grid.Children.Add(grid);
+            Grid.SetColumn(grid, 1);
         }
     }
 }
